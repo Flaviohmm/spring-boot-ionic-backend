25863b789faa5aeb3457fc15e47fd5f858e6a2e0
Flaviohmm <fhenrique609@gmail.com>
2021-01-26 11:39:41 -0300

    Subtotal e Total do Pedido

--- src/main/java/com/flaviomacedo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/flaviomacedo/cursomc/domain/ItemPedido.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
@@ -30,12 +30,16 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
