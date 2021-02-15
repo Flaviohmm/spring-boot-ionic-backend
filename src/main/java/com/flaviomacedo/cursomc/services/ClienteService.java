19b9b08c67e3c28bd934526e4552d16da28c4f83
Flaviohmm <fhenrique609@gmail.com>
2021-02-15 15:46:07 -0300

    Usando Padrão de Nomes para Imagens

--- src/main/java/com/flaviomacedo/cursomc/services/ClienteService.java
+++ src/main/java/com/flaviomacedo/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.flaviomacedo.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -39,17 +41,23 @@ public class ClienteService {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private S3Service s3Service;
 
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
+
 	public Cliente find(Integer id) {
-		
+
 		UserSS user = UserService.authenticated();
 		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso Negado");
 		}
-		
+
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
@@ -112,19 +120,17 @@ public class ClienteService {
 		newObjeto.setNome(objeto.getNome());
 		newObjeto.setEmail(objeto.getEmail());
 	}
-	
+
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		
+
 		UserSS user = UserService.authenticated();
 		if (user == null) {
 			throw new AuthorizationException("Acesso Negado");
 		}
-		URI uri = s3Service.uploadFile(multipartFile);
+
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 		
-		Cliente cliente = find(user.getId());
-		cliente.setImageUrl(uri.toString());
-		repository.save(cliente);
-		
-		return uri;
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
---
+++ src/main/java/com/flaviomacedo/cursomc/services/ImageService.java
@@ -0,0 +1,52 @@
+package com.flaviomacedo.cursomc.services;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.flaviomacedo.cursomc.services.exceptions.FileException;
+
+@Service
+public class ImageService {
+
+	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
+		String ext = FilenameUtils.getExtension(uploadedFile.getOriginalFilename());
+		if (!"png".equals(ext) && !"jpg".equals(ext)) {
+			throw new FileException("Somente imagens PNG e JPG são permitidas");
+		}
+		try {
+			BufferedImage image = ImageIO.read(uploadedFile.getInputStream());
+			if ("png".equals(ext)) {
+				image = pngToJpg(image);
+			}
+			return image;
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler arquivo");
+		}
+	}
+
+	public BufferedImage pngToJpg(BufferedImage image) {
+		BufferedImage jpgImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+		jpgImage.createGraphics().drawImage(image, 0, 0, Color.WHITE, null);
+		return jpgImage;
+	}
+	
+	public InputStream getInputStream(BufferedImage image, String extension) {
+		try {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			ImageIO.write(image, extension, outputStream);
+			return new ByteArrayInputStream(outputStream.toByteArray());
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler Arquivo");
+		}
+	}
+}
