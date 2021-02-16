ff7d5171716e4ff38cf48a674a757e4dfdcd643f
Flaviohmm <fhenrique609@gmail.com>
2021-02-16 11:56:39 -0300

    Endpoint para buscar Cliente por Email

--- src/main/java/com/flaviomacedo/cursomc/services/ClienteService.java
+++ src/main/java/com/flaviomacedo/cursomc/services/ClienteService.java
@@ -47,10 +47,10 @@ public class ClienteService {
 
 	@Autowired
 	private ImageService imageService;
-	
+
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
-	
+
 	@Value("${img.profile.size}")
 	private Integer size;
 
@@ -91,6 +91,20 @@ public class ClienteService {
 
 	public List<Cliente> findAll() {
 		return repository.findAll();
+	}
+
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		Cliente objeto = repository.findByEmail(email);
+		if (objeto == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return objeto;
 	}
 
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
@@ -134,9 +148,9 @@ public class ClienteService {
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
 		jpgImage = imageService.cropSquare(jpgImage);
 		jpgImage = imageService.resize(jpgImage, size);
-		
+
 		String fileName = prefix + user.getId() + ".jpg";
-		
+
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
